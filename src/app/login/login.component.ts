9ac98be49575bee2003e36d40d6033222b3354ed
Nikki Pacatang <npacatang89487@liceo.edu.ph>
2026-03-06 00:29:58 +0800

    login

---
+++ src/app/login/login.component.ts
@@ -0,0 +1,22 @@
+import { Component } from '@angular/core';
+import { CommonModule } from '@angular/common';
+import { FormsModule } from '@angular/forms';
+
+@Component({
+  selector: 'app-login',
+  standalone: true,
+  imports: [CommonModule, FormsModule],
+  templateUrl: './login.component.html',
+  styleUrls: ['./login.component.scss']
+})
+export class LoginComponent {
+
+  email: string = '';
+  password: string = '';
+
+  login() {
+    console.log('Email:', this.email);
+    console.log('Password:', this.password);
+  }
+
+}
